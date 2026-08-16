c287aa39e40632897bdf605775e9db33b32ab671
mingodando <thebestballer182@gmail.com>
2026-08-16 11:13:11 +0700

    add the different models into what the products and category will be about

--- pc-building/store/models.py
+++ pc-building/store/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+class ComponentType(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    component_type = models.ForeignKey(ComponentType, on_delete=models.CASCADE, null=True, blank=True)
+
+    CONDITION_CHOICES = [
+        ('new', 'New'),
+        ('refurbished', 'Refurbished'),
+    ]
+
+    condition = models.CharField(choices=CONDITION_CHOICES, max_length=20, default='new')
+
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+
+    GRADE_CHOICES = [
+        ('A', 'Grade A'),
+        ('B', 'Grade B'),
+        ('C', 'Grade C'),
+    ]
+
+    grade = models.CharField(choices=GRADE_CHOICES, max_length=5, blank=True, null=True)
