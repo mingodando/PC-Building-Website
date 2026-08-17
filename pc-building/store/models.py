0964fd2f3ea2672d11c2d764c57342f9e8311f78
mingodando <thebestballer182@gmail.com>
2026-08-17 09:17:51 +0700

    updated the product class and added the Spec details model

--- pc-building/store/models.py
+++ pc-building/store/models.py
@@ -31,6 +31,11 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     component_type = models.ForeignKey(ComponentType, on_delete=models.CASCADE, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    compare_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    stock = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products', null=True, blank=True)
+    is_active = models.BooleanField(default=True)
 
     CONDITION_CHOICES = [
         ('new', 'New'),
@@ -48,3 +53,19 @@ class Product(models.Model):
     ]
 
     grade = models.CharField(choices=GRADE_CHOICES, max_length=5, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+class SpecDetails(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='specs')
+    key = models.CharField(max_length=30)
+    value = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.product} - {self.key}: {self.value}"
