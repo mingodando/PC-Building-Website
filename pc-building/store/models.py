6884258be46af7b659dc65f23e6ca94122298209
mingodando <thebestballer182@gmail.com>
2026-08-18 13:49:33 +0700

    added a few mroe models, for ordering and putting items in cart

--- pc-building/store/models.py
+++ pc-building/store/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.text import slugify
 
@@ -71,3 +72,53 @@ class SpecDetails(models.Model):
 
     def __str__(self):
         return f"{self.product} - {self.key}: {self.value}"
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart of {self.user.username}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        unique_together = ('cart', 'product')
+
+    def __str__(self):
+        return f"{self.product} x {self.quantity} in {self.cart}"
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('paid', 'Paid'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    DELIVERY_CHOICES = [
+        ('standard', 'Standard Delivery'),
+        ('express', 'Express Delivery'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=20, default='pending')
+    payment_reference = models.CharField(max_length=100, blank=True, null=True, unique=True)
+    delivery_method = models.CharField(choices=DELIVERY_CHOICES, max_length=20, default='standard')
+
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(CartItem, default=11)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
