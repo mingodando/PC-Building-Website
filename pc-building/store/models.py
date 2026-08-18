6e83d7e4d87ec765b9d463eab40ed069f2556bde
mingodando <thebestballer182@gmail.com>
2026-08-18 14:09:16 +0700

    fixed up a few of hte models pluralizations??? and edited the urls.py to make sure error doesnt occur and added the admin section

--- pc-building/store/models.py
+++ pc-building/store/models.py
@@ -11,6 +11,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
@@ -73,6 +76,9 @@ class SpecDetails(models.Model):
     def __str__(self):
         return f"{self.product} - {self.key}: {self.value}"
 
+    class Meta:
+        verbose_name_plural = "Spec Details"
+
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -120,5 +126,8 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(CartItem, default=11)
+    quantity = models.PositiveIntegerField(default=1)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product} x {self.quantity} in {self.order}"
