4cfcce95ff848ebbcd404149eabae78385778a08
mingodando <thebestballer182@gmail.com>
2026-08-18 14:09:28 +0700

    fixed up a few of hte models pluralizations??? and edited the urls.py to make sure error doesnt occur and added the admin section also made migrations

--- pc-building/store/admin.py
+++ pc-building/store/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, ComponentType, Product, SpecDetails, Cart, CartItem, Order, OrderItem
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(ComponentType)
+admin.site.register(Product)
+admin.site.register(SpecDetails)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
+admin.site.register(Order, OrderAdmin)
