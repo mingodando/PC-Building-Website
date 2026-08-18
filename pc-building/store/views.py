3646e0d3dc621888403f136cdaf5ef426fba51ef
mingodando <thebestballer182@gmail.com>
2026-08-18 18:51:27 +0700

    addded a more details page for each compoennt

--- pc-building/store/views.py
+++ pc-building/store/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 def product_list(request):
     products = Product.objects.filter(is_active=True)
     return render(request, 'store/product_list.html', {'products': products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, is_active=True)
+    return render(request, 'store/product_detail.html', {'product': product})
